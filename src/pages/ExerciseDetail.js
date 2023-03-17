9d22ea1eaf3b0edf4049a952ae06d38e215b9316
pavelFaraday <devstudio.corp@gmail.com>
2023-03-17 21:44:43 +0400

    start detail screen / create new components

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+import Detail from "../components/Detail";
+import ExerciseVideos from "../components/ExerciseVideos";
+import SimilarExercise from "../components/SimilarExercises";
+import { exercisesOptions, fetchData } from "../utils/fetchData";
 
 const ExerciseDetail = () => {
-	return <div>ExerciseDetail</div>;
+	const [exerciseDetail, setExerciseDetail] = useState({});
+	const { id } = useParams();
+
+	useEffect(() => {
+		const fetchExercisesData = async () => {
+			const exerciseDbUrl = "https://exercisedb.p.rapidapi.com";
+			const youtubeSearchUrl =
+				"https://youtube-search-and-download.p.rapidapi.com";
+			const exerciseDetailData = await fetchData(
+				`${exerciseDbUrl}/exercises/${id}`,
+				exercisesOptions
+			);
+			setExerciseDetail(exerciseDetailData);
+		};
+		fetchExercisesData();
+	}, [id]);
+
+	return (
+		<Box>
+			<Detail exerciseDetail={exerciseDetail} />
+			<ExerciseVideos />
+			<SimilarExercise />
+		</Box>
+	);
 };
 
 export default ExerciseDetail;
